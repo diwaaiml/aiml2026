c28b623af74d94efedeac9a2232eefd24f60e654
Diwakar <you@example.com>
2026-03-07 20:45:24 +0530

    New Changes -2

--- phase0/class.py
+++
@@ -1,9 +0,0 @@
-class Name:
-    def __init__(self, name):
-        self.name = name
-    def bark(self,a,b="rAJI"):
-        print(a+self.name+b)
-        
-name1 = Name("Loves")
-name1.bark("DiWA")
-
